6a37f0a245268027b0b86dabae4114ff3934cdba
“Utkarsh-Ashish” <UtkarshAgOfficial@gmail.com>
2025-04-13 15:42:19 +0530

    updated comments

--- src/main/java/com/example/ProductService/ProductServiceApplication.java
+++ src/main/java/com/example/ProductService/ProductServiceApplication.java
@@ -16,5 +16,5 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
+	// help to check ssh working fine or not
 }
